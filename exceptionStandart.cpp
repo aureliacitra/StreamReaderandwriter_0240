0892c1ae0f78fabd9a25619cda3f29f25a41235a
aureliacitra <citrapangukir@gmail.com>
2025-06-09 19:00:19 +0700

    add catch exception

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -11,5 +11,9 @@ int main ()
         array<int, 3> data = {10, 20, 30};
         cout << data.at(4) << endl;
     }
-
+    catch(exception &e)
+    {
+        cout << e.what() << endl;
+    }
+    
 }

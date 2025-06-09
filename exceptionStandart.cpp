a40483c54302c711e6f579d7bacaae06a8b43022
aureliacitra <citrapangukir@gmail.com>
2025-06-09 18:58:00 +0700

    add int main dan try

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <exception>
 #include <array>
+using namespace std;
+
+int main ()
+{
+    cout << "awal program" << endl;
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        cout << data.at(4) << endl;
+    }
+
+}
